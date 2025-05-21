d41388bbf7519060a5710807edc00c54c6dd4ee4
mfurqaann <furqan1061@gmail.com>
2025-05-21 11:55:32 +0700

    refactor(onlinestore): add port on createPool

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
+    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
