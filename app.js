8b71b27294507760d508a8c5cee16708307dae31
mfurqaann <furqan1061@gmail.com>
2025-05-21 11:03:09 +0700

    refactor(onlinestore): change order middleware on app.js

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json());
 app.use(cors({
     origin: 'https://store-project-gilt.vercel.app',
     credentials: true
 }));
-app.use(cookieParser())
+
+app.use(cookieParser());
+app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
