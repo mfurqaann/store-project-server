01a7baff56f1ae3f9a8a91ea38d3c622d9893c4f
mfurqaann <furqan1061@gmail.com>
2025-05-21 12:22:57 +0700

    refactor(onlinestore): test db

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/api/auth', authRoutes);
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '✅' : '❌');
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_NAME:', process.env.DB_NAME);
 
 
