0d31d747e47491f2572a2cde4375760aa87ca7fc
mfurqaann <furqan1061@gmail.com>
2025-05-21 10:58:21 +0700

    refactor(onlinestore): change origin url cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://store-project-gilt.vercel.app/',
+    origin: 'https://store-project-gilt.vercel.app',
     credentials: true
 }));
 app.use(cookieParser())
