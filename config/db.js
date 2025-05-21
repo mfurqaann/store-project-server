3dedf715ebf7c5cee4558d2cb20e426495735d79
mfurqaann <furqan1061@gmail.com>
2025-05-21 12:25:19 +0700

    refactor(onlinestore): test db

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
