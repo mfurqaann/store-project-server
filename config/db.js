abcdb891643c1a116f2f2cd9bb094251b6c47f33
mfurqaann <furqan1061@gmail.com>
2025-05-26 17:43:45 +0700

    refactor(onlinestore): change environment variable

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-    port: Number(process.env.DB_PORT),
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    port: Number(process.env.MYSQLPORT),
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 });
 
 
